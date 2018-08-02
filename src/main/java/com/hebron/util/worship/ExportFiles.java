01b862d9c0561a933788d36acdbd6ad9d4232172
danybenjamin <dany.benjamin@gmail.com>
2018-08-02 11:00:49 -0500

    Add a java class and a test class

---
+++ src/main/java/com/hebron/util/worship/ExportFiles.java
@@ -0,0 +1,11 @@
+package com.hebron.util.worship;
+
+public class ExportFiles {
+
+    public static void main (String [] args){
+        System.out.println("Hello IPC");
+
+        //Do Stuff here!
+    }
+
+}
---
+++ src/test/java/com/hebron/util/worship/ExportFilesTest.java
@@ -0,0 +1,12 @@
+package com.hebron.util.worship;
+import org.junit.Test;
+
+public class ExportFilesTest {
+
+    @Test
+    public void sayHello(){
+        //Expand this later
+
+        assert(true);
+    }
+}
